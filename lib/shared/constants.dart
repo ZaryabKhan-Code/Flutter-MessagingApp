bdf5c0cc6b25e8f2046aba79e32fcb23e5483be1
Zaryab Khan <87335498+ZaryabKhan-Code@users.noreply.github.com>
2022-09-04 02:49:06 +0500

    Add files via upload
    desktop upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class Constants{
+   static String appId="1:682779643916:web:66a56c617927cdcc0c5e29";
+   static String apiKey="AIzaSyC8S6jZ4_Cz1MPzMeLVfAR4a4D2JOOuZGA";
+   static String messagingSenderId="682779643916";
+   static String projectId="cheetify-49963";
+   final primaryColor = const Color(0xFFee7b64);
+}
